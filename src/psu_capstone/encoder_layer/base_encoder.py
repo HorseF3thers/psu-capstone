2e6be7f478fadb30d3fa199edbf1e2fde091261e
millscb <40242984+millscb@users.noreply.github.com>
2025-11-12 19:09:46 -0600

    Sdr (#19)
    Co-authored-by: Eric Grembocki <egrembocki@gmail.com>

--- src/psu_capstone/encoder_layer/base_encoder.py
+++ src/psu_capstone/encoder_layer/base_encoder.py
@@ -1,51 +1,36 @@
-"""Base class for all encoder types."""
-
 from abc import ABC, abstractmethod
 from typing import List
-
-from sympy import prod
-from typing_extensions import Self
 
 from psu_capstone.encoder_layer.sdr import SDR
 
 
 class BaseEncoder(ABC):
 
-    # private members
-    __size: int
-    """Scalar size of the encoder output."""
-
-    __dimensions: List[int]
-    """Input space of the encoder."""
-
-    __sdr: SDR
-    """Internal SDR representation."""
-
-    def __new__(cls) -> Self:
-        cls.__sdr = SDR([1])
-        cls.__size = 0
-        cls.__dimensions = []
-        return super().__new__(cls)
-
     def __init__(self, dimensions: List[int]):
-        self.__dimensions = dimensions
-        self.__size = prod(int(dim) for dim in self.__dimensions)
+        self._dimensions: List[int] = []
+        self._size: int = 0
 
-        print(
-            f"Initialized BaseEncoder with dimensions: {self.__dimensions}"
-            f" and size: {self.__size}"
-        )
+        if dimensions is not None:
+            self.initialize(dimensions)
 
+    #
+    # Members dimensions & size describe the shape of the encoded output SDR.
+    # This is the total number of bits in the result.
+    #
     @property
     def dimensions(self) -> List[int]:
-        return self.__dimensions
+        return self._dimensions
 
     @property
     def size(self) -> int:
-        return self.__size
+        return self._size
+
+    def initialize(self, dimensions: List[int]):
+        self._dimensions = list(dimensions)
+        self._size = SDR(dimensions).size
 
     def reset(self):
-        raise NotImplementedError("Not implemented yet")
+        pass
 
     @abstractmethod
     def encode(self, input_value, output):
